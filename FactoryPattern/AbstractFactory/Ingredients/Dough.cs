f8c347921a39de4c183f7a918e9e57d435a6a415
greggsjh <fundigent@gmail.com>
2021-02-05 22:42:36 -0500

    Cleaning up

--- FactoryPattern/AbstractFactory/Ingredients/Dough.cs
+++
@@ -1,8 +0,0 @@
-namespace DesignPatterns.FactoryPattern.AbstractFactory
-{
-    public class Dough
-    {
-        
-    }
-    
-}
--- FactoryPattern/AbstractFactory/Ingredients/Veggie.cs
+++
@@ -1,6 +0,0 @@
-namespace DesignPatterns.FactoryPattern.AbstractFactory
-{
-    public class Veggie
-    {
-    }
-}
